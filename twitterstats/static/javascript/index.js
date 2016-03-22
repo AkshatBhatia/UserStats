d587bcd8f3f405cd719c537b33ce4583d2703364
Akshat Bhatia <akshatbhatia@gmail.com>
2016-03-22 13:21:56 -0700

    Added pie chart.

--- twitterstats/static/javascript/index.js
+++ twitterstats/static/javascript/index.js
@@ -26,6 +26,19 @@ app.Tweets = Backbone.Collection.extend({
 
 app.tweets = new app.Tweets();
 
+app.Summary = Backbone.Model.extend({
+    defaults: {
+        original_tweet_count: "original_tweet_count",
+        retweet_count: "retweet_count",
+        replies_count: "replies_count",
+        tweets_with_hashtags: "tweets_with_hashtags",
+        tweets_with_mentions: "tweets_with_mentions"
+    },
+    url: '/twitterstats/summary'
+});
+
+app.summary = new app.Summary();
+
 // Views
 app.TweetView = Backbone.View.extend({
     tagName: 'tr',
@@ -37,138 +50,194 @@ app.TweetView = Backbone.View.extend({
 });
 
 app.AppView = Backbone.View.extend({
-    el: '#twitterstatsapp',
-    userId: '#user',
-    filtersId: '#filters option:selected',
-    user: '',
-    headers: _.template('<tr><th>TweetId</th><th>Author</th><th>Text</th><th data-defaultsort="desc">Favorites</th><th>Retweets</th></tr>'),
+      el: '#twitterstatsapp',
+      userId: '#user',
+      filtersId: '#filters option:selected',
+      user: '',
+      headers: _.template('<tr><th>TweetId</th><th>Author</th><th>Text</th><th data-defaultsort="desc">Favorites</th><th>Retweets</th></tr>'),
 
-    events: {
-        "click .search": "searchUser",
-    },
-    initialize: function () {
-        app.tweets.on('reset', this.addAll, this);
-        this.fetchTweets();
-    },
-    fetchTweets: function() {
-        var that = this;
-        app.tweets.fetch({data: {user: this.user,
-            filters: this.filters},
-            success: function () {
-                drawGraph();
-                that.addAll();
-            }});
-    },
-    addOne: function(tweet){
-        var view = new app.TweetView({model: tweet});
-        this.$('#tweet-list').append(view.render().el);
-    },
-    addAll: function(){
-        console.log("Reset is called");
-        this.$('#tweet-list').empty();
-        app.tweets.each(this.addOne, this);
-    },
-    searchUser: function(){
-        this.user = $(this.userId).val();
-        var filterArray = _.map($(this.filtersId), function (d) {
-            return { name: $(d).parent().attr('label'),
-                value : d.text};
-        });
+      events: {
+          "click .search": "searchUser",
+      },
+      initialize: function () {
+          app.tweets.on('reset', this.addAll, this);
+          this.fetchTweets();
+      },
+      fetchTweets: function () {
+          var that = this;
+          app.tweets.fetch({
+              data: {
+                  user: this.user,
+                  filters: this.filters
+              },
+              success: function () {
+                  that.drawGraph();
+                  that.addAll();
+              }
+          });
+          app.summary.fetch({
+              data: {
+                  user: this.user,
+                  filters: this.filters
+              },
+              success: function () {
+                  that.drawPieChart();
+              }
+          });
+      },
+      addOne: function (tweet) {
+          var view = new app.TweetView({model: tweet});
+          this.$('#tweet-list').append(view.render().el);
+      },
+      addAll: function () {
+          console.log("Reset is called");
+          this.$('#tweet-list').empty();
+          app.tweets.each(this.addOne, this);
+      },
+      searchUser: function () {
+          this.user = $(this.userId).val();
+          var filterArray = _.map($(this.filtersId), function (d) {
+              return {
+                  name: $(d).parent().attr('label'),
+                  value: d.text
+              };
+          });
 
-        var x = _.groupBy(filterArray, function(d) {
-            return d.name;
-        })
+          var x = _.groupBy(filterArray, function (d) {
+              return d.name;
+          })
 
-        console.log(x);
+          console.log(x);
 
-        this.filters = _.map(_.allKeys(x), function(d) {
-            var vall = _.map(x[d], function(d) {return d['value']});
-            return d.toString() + '=' + vall; }).join(";");
+          this.filters = _.map(_.allKeys(x), function (d) {
+              var vall = _.map(x[d], function (d) {
+                  return d['value']
+              });
+              return d.toString() + '=' + vall;
+          }).join(";");
 
-        console.log(this.filters);
+          console.log(this.filters);
 
-        this.fetchTweets();
-    }
-});
+          this.fetchTweets();
+      },
+      drawGraph: function () {
+          var retweet_data = app.tweets.toJSON().map(function (d) {
+              return [d.created_at, d.retweet_count];
+          }).sort();
+          var favourite_data = app.tweets.toJSON().map(function (d) {
+              return [d.created_at, d.favorite_count];
+          }).sort();
+
+          // create the chart
+          return $('#chart1').highcharts('StockChart', {
+              chart: {
+                  alignTicks: false
+              },
+              colors: ['#7cb5ec', '#90ed7d', '#8085e9', '#f7a35c',
+                  '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'],
+              rangeSelector: {
+                  selected: 5
+              },
+
+              title: {
+                  text: 'Retweet chart'
+              },
+
+              series: [{
+                  type: 'line',
+                  name: 'Retweet count',
+                  data: retweet_data,
+                  dataGrouping: {
+                      units: [[
+                          'week', // unit name
+                          [1] // allowed multiples
+                      ], [
+                          'month',
+                          [1, 2, 3, 4, 6]
+                      ]]
+                  }
+              }, {
+                  type: 'line',
+                  name: 'Favourite count',
+                  data: favourite_data,
+                  dataGrouping: {
+                      units: [[
+                          'week', // unit name
+                          [1] // allowed multiples
+                      ], [
+                          'month',
+                          [1, 2, 3, 4, 6]
+                      ]]
+                  }
+              }]
+          });
+      },
+      drawPieChart: function(){
+          var summaryJson = app.summary.toJSON();
+          var colorsArray = ['#7cb5ec', '#90ed7d', '#f7a35c', '#8085e9',
+              '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'];
+          $('#pie_chart').highcharts({
+              chart: {
+                  plotBackgroundColor: null,
+                  plotBorderWidth: null,
+                  plotShadow: false,
+                  type: 'pie'
+              },
+              colors: Highcharts.map(colorsArray, function (color) {
+                  return {
+                      radialGradient: {
+                          cx: 0.5,
+                          cy: 0.3,
+                          r: 0.7
+                      },
+                      stops: [
+                          [0, color],
+                          [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+                      ]
+                  };
+              }),
+              title: {
+                  text: 'Tweet Summary'
+              },
+              tooltip: {
+                  pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+              },
+              plotOptions: {
+                  pie: {
+                      allowPointSelect: true,
+                      cursor: 'pointer',
+                      dataLabels: {
+                          enabled: true,
+                          format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                          style: {
+                              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                          },
+                          connectorColor: 'silver'
+                      }
+                  }
+              },
+              series: [{
+                  name: 'Tweet Summary',
+                  data: [
+                      {
+                          name: 'Original Tweets',
+                          y: summaryJson.original_tweet_count,
+                          sliced: true,
+                          selected: true
+                      },
+                      {
+                          name: 'Retweets',
+                          y: summaryJson.retweet_count
+                      },
+                      {
+                          name: 'Replies',
+                          y: summaryJson.replies_count
+                      }
+                  ]
+              }]
+          });
+      }
+  }
+);
 
 var appView = new app.AppView();
-
-function drawGraph() {
-    var retweet_data = app.tweets.toJSON().map(function(d){ return [d.created_at, d.retweet_count]; }).sort();
-    var favourite_data = app.tweets.toJSON().map(function(d){ return [d.created_at, d.favorite_count]; }).sort();
-
-    // create the chart
-    return $('#chart1').highcharts('StockChart', {
-        chart: {
-            alignTicks: false
-        },
-        colors: ['#7cb5ec', '#90ed7d', '#f7a35c', '#8085e9',
-            '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'],
-        rangeSelector: {
-            selected: 5
-        },
-
-        title: {
-            text: 'Retweet chart'
-        },
-
-        series: [{
-            type: 'line',
-            name: 'Retweet count',
-            data: retweet_data,
-            dataGrouping: {
-                units: [[
-                    'week', // unit name
-                    [1] // allowed multiples
-                ], [
-                    'month',
-                    [1, 2, 3, 4, 6]
-                ]]
-            }
-        },{
-            type: 'line',
-            name: 'Favourite count',
-            data: favourite_data,
-            dataGrouping: {
-                units: [[
-                    'week', // unit name
-                    [1] // allowed multiples
-                ], [
-                    'month',
-                    [1, 2, 3, 4, 6]
-                ]]
-            }
-        }]
-    });
-}
-
-function retweet_ts() {
-    return app.tweets.toJSON()
-        .map(function(d) {
-            return {x: d.created_at, y: d.retweet_count};
-        }).reverse();
-}
-
-function favorite_ts() {
-    return app.tweets.toJSON()
-        .map(function(d) {
-            return {x: d.created_at, y: d.favorite_count};
-        }).reverse();
-}
-
-function tweetData() {
-    return [
-        {
-            key: 'Retweet Count',
-            values: retweet_ts()
-        },
-        {
-            key: 'Favorite Count',
-            values: favorite_ts()
-        }
-    ];
-}
-
-function xDomain() {
-    return retweet_ts().map(function(d) { return d.x; });
-}
