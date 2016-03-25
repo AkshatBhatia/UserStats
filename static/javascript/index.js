349645826d513c0cf16027011d1b7eae28813593
Akshat Bhatia <akshatbhatia@gmail.com>
2016-03-24 18:18:13 -0700

    Added new static files.

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -9,9 +9,18 @@ app.Tweet = Backbone.Model.extend({
     defaults: {
         id_str: "id_str",
         author: "Author",
+        created_at: "created_at",
         text: "This is a tweet text",
         favorite_count: "favorite_count",
         retweet_count: "retweet_count",
+    },
+    parse: function(data) {
+        var date = new Date(data.created_at);
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+        data.created_date = monthIndex + '-' + day + '-' + year;
+        return data;
     }
 });
 
@@ -59,6 +68,14 @@ app.user = new app.User();
 app.UserView = Backbone.View.extend({
     tagName: 'h1',
     template: _.template($('#header-template').html()),
+    render: function() {
+        this.$el.html(this.template(app.user.toJSON()));
+        return this;
+    }
+})
+
+app.UserOverview = Backbone.View.extend({
+    template: _.template($('#user-overview-template').html()),
     render: function(){
         this.$el.html(this.template(this.model.toJSON()));
         return this;
@@ -75,13 +92,15 @@ app.TweetView = Backbone.View.extend({
     }
 });
 
+var ENTER_KEY = 13;
 app.AppView = Backbone.View.extend({
-      el: '#twitterstatsapp',
+      el: $('.input-group'),
       userId: '#user',
       filtersId: '#filters option:selected',
       user: '',
       events: {
-          "click .search": "searchUser",
+          'click #search-user': 'searchUser',
+          'keydown' : 'keyPressEventHandler'
       },
       initialize: function () {
           app.tweets.on('reset', this.addAll, this);
@@ -90,8 +109,11 @@ app.AppView = Backbone.View.extend({
       },
       fetchUser: function() {
           app.user.fetch({
+              data: {
+                user: this.user
+              },
               success: function(){
-                  var view = new app.UserView({model: app.user});
+                  var view = new app.UserView();
                   this.$('#header').html(view.render().el);
               }
           });
@@ -115,7 +137,7 @@ app.AppView = Backbone.View.extend({
               },
               success: function () {
                   var userOverview = new app.UserOverview({model: app.summary});
-                  debugger;
+                  this.$('#user-overview').empty();
                   this.$('#user-overview').append(userOverview.render().el);
                   that.drawPieChart();
               }
@@ -123,12 +145,17 @@ app.AppView = Backbone.View.extend({
       },
       addOne: function (tweet) {
           var view = new app.TweetView({model: tweet});
-          this.$('#tweet-list').append(view.render().el);
+          $('#tweet-list').append(view.render().el);
       },
       addAll: function () {
-          console.log("Reset is called");
-          this.$('#tweet-list').empty();
+          $('#tweet-list').empty();
           app.tweets.each(this.addOne, this);
+      },
+      keyPressEventHandler: function(e) {
+          console.log("here " + e.which);
+          if(e.which === ENTER_KEY) {
+            this.searchUser();
+          }
       },
       searchUser: function () {
           this.user = $(this.userId).val();
@@ -143,8 +170,6 @@ app.AppView = Backbone.View.extend({
               return d.name;
           })
 
-          console.log(x);
-
           this.filters = _.map(_.allKeys(x), function (d) {
               var vall = _.map(x[d], function (d) {
                   return d['value']
@@ -152,12 +177,11 @@ app.AppView = Backbone.View.extend({
               return d.toString() + '=' + vall;
           }).join(";");
 
-          console.log(this.filters);
-
           this.fetchUser();
           this.fetchTweets();
       },
       drawGraph: function () {
+          console.log("inside drawgraph");
           var retweet_data = app.tweets.toJSON().map(function (d) {
               return [d.created_at, d.retweet_count];
           }).sort();
@@ -169,6 +193,9 @@ app.AppView = Backbone.View.extend({
           return $('#chart1').highcharts('StockChart', {
               chart: {
                   alignTicks: false
+              },
+              title: {
+                  text: "Likes and Retweets timeseries"
               },
               colors: ['#7cb5ec', '#90ed7d', '#8085e9', '#f7a35c',
                   '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'],
